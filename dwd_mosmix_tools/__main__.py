9759e8faf62599b82c2f19d67ae9081604feb766
Björn Reetz <bjoern.reetz@dwd.de>
2022-06-12 08:09:49 +0000

    fixed pat of test file

--- dwd_mosmix_tools/__main__.py
+++ dwd_mosmix_tools/__main__.py
@@ -6,5 +6,5 @@ from dwd_mosmix_tools.kml2json import kml2geojson
 
 if __name__ == "__main__":
     max_stations = 7
-    geojson = kml2geojson("../data/MOSMIX_L_2022061021.kml", max_stations)
+    geojson = kml2geojson("data/MOSMIX_L_2022061021.kml", max_stations)
     json.dump(geojson, sys.stdout, indent=2)
